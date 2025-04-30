bce33c02b1451229ae3d620edfb93e9fd7457fbd
shelvinx <shelvinx@outlook.com>
2025-04-30 18:52:36 +0100

    avm naming

--- avm.kv.tf
+++ avm.kv.tf
@@ -3,7 +3,7 @@ module "kv" {
     source = "Azure/avm-res-keyvault-vault/azurerm"
     version = "0.10.0"
 
-    name = module.naming.keyvault.name
+    name = module.naming.key_vault.name
     location = var.location
     resource_group_name = var.resource_group_name
     tenant_id = data.azurerm_client_config.this.tenant_id
