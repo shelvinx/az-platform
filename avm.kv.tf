78b5d9435381d154cbc2228817fe6866fbeffd7c
shelvinx <shelvinx@outlook.com>
2025-04-30 18:42:57 +0100

    add kv

--- avm.kv.tf
+++ avm.kv.tf
@@ -1,1 +1,10 @@
 # AVM Module for Azure Key Vault
+module "kv" {
+    source = "Azure/avm-res-keyvault-vault/azurerm"
+    version = "0.10.0"
+
+    name = "module.naming.keyvault.name"
+    location = var.location
+    resource_group_name = var.resource_group_name
+    tenant_id = var.ARM_TENANT_ID
+}
