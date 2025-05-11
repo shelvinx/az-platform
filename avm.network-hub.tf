a979552197fd35411e3c6de4a405cf543bac20a9
shelvinx <shelvinx@outlook.com>
2025-05-11 20:33:10 +0100

    nsg rules

--- avm.network-hub.tf
+++ avm.network-hub.tf
@@ -25,7 +25,57 @@ module "vnet_hub" {
      Default = {
         name = "default"
         address_prefix = "10.0.3.0/24"
+        network_security_group = {
+            id = "${module.nsg_default.resource_id}"
+        }
      }
+    }
+
+    tags = var.tags
+}
+
+module "nsg_default" {
+    source = "Azure/avm-res-network-networksecuritygroup/azurerm"
+    version = "0.4.0"
+
+    name = "nsg-default"
+    location = var.location
+    resource_group_name = var.resource_group_name
+
+    security_rules = {
+        AllowSSH = {
+            name = "AllowSSH"
+            priority = 100
+            direction = "Inbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = "22"
+            source_address_prefix = "*"
+            destination_address_prefix = "*"
+        }
+        AllowHTTP = {
+            name = "AllowHTTP"
+            priority = 101
+            direction = "Inbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = "80"
+            source_address_prefix = "*"
+            destination_address_prefix = "*"
+        }
+        AllowHTTPS = {
+            name = "AllowHTTPS"
+            priority = 102
+            direction = "Inbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = "443"
+            source_address_prefix = "*"
+            destination_address_prefix = "*"
+        }
     }
 
     tags = var.tags
