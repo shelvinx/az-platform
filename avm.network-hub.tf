0e48a87652ee4be3e833c6d82994fcd1b8757e0a
shelvinx <shelvinx@outlook.com>
2025-05-11 17:29:51 +0100

    feat: hub vnet

---
+++ avm.network-hub.tf
@@ -0,0 +1,28 @@
+# AVM for HUB Network
+module "vnet_hub" {
+    source = "Azure/avm-res-network-virtualnetwork/azurerm"
+    version = "0.8.1"
+
+    name = "vnet-hub"
+    location = var.location
+    resource_group_name = var.resource_group_name
+
+    address_space = ["10.0.0.0/16"]
+
+    subnets = {
+     AzureFirewallSubnet = {
+        name = "AzureFirewallSubnet"
+        address_prefix = "10.0.0.0/27"
+     }
+     GatewaySubnet = {
+        name = "GatewaySubnet"
+        address_prefix = "10.0.1.0/27"
+     }
+     AzureBastionSubnet = {
+        name = "AzureBastionSubnet"
+        address_prefix = "10.0.2.0/27"
+     }
+    }
+
+    tags = var.tags
+}
